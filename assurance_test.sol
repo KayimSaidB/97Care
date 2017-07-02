99e1218b590080dec90050e26ef1a83e0d274bde
KayimSaidB <kayimsaid@hotmail.fr>
2017-07-02 21:14:26 +0200

    gestion validation par un tiers

--- assurance_test.sol
+++ assurance_test.sol
@@ -7,6 +7,8 @@ contract assurance_test{
    
     mapping (address => bool[3]) registre_assure; /// indique si une personne est dans le registre
     mapping (address => uint256) compte_assure;
+    mapping (address => bool) registre_specialiste; // registre des personnes qui valident
+    /// les déclarations de sinistre
     /// tableau de boolens pour indiquer si elle a payé ses droits , on peut imaginer un tableau plus grand
     // pour gérer différents types de prestations
     function adhesion() returns (bool){
@@ -18,12 +20,38 @@ contract assurance_test{
         else 
         return false;
         }
-    
+    function ajouter_specialiste(address nouveau){
+        
+        registre_specialiste[nouveau]=true;
+    }
+
+    struct un_sinistre{
+        uint type_sinistre;
+        uint valeur_sinistre;
+        bool est_valide;
+    }
+    mapping (uint256 => un_sinistre) registre_sinistre; // chaque sinistre va etre reconnu par un id
     function recharger_compte() payable{
         compte_assure[msg.sender]+=msg.value;
     }
     function check_compte()returns (uint){
         return compte_assure[msg.sender];
+    }
+    uint compteur_sinistre=1;
+    function declaration_sinistre_au_specialiste(uint type_sinistre,uint valeur_sinistre) returns (bool){
+        if (registre_assure[msg.sender][type_sinistre]==true){
+            registre_sinistre[compteur_sinistre].type_sinistre=type_sinistre;
+            registre_sinistre[compteur_sinistre].valeur_sinistre=valeur_sinistre;
+            compteur_sinistre++;
+            return true;
+            }
+        else 
+        return false;
+    }
+    
+    function validation_specialiste(uint reference_sinistre, bool choix) returns (bool){
+        if (registre_specialiste[msg.sender]==true) registre_sinistre[reference_sinistre].est_valide=choix;
+        return choix;
     }
     function cotisation(uint choix_assurance) payable returns (bool){
     if (compte_assure[msg.sender]>=prix){
@@ -42,10 +70,10 @@ contract assurance_test{
         return tresorerie;
     }
 
-    function declaration_sinistre_apres_validation(uint type_sinistre, uint valeur_sinistre){
-    if (registre_assure[msg.sender][type_sinistre]==true && tresorerie>=valeur_sinistre){
-        compte_assure[msg.sender]+=valeur_sinistre;
-        tresorerie-=valeur_sinistre;
+    function declaration_sinistre_apres_validation(uint reference_sinistre){
+    if (registre_sinistre[reference_sinistre].est_valide==true && tresorerie>=registre_sinistre[reference_sinistre].valeur_sinistre){
+        compte_assure[msg.sender]+=registre_sinistre[reference_sinistre].valeur_sinistre;
+        tresorerie-=registre_sinistre[reference_sinistre].valeur_sinistre;
     }
     }
 
